f6dcb827c8afc10f4685c3c1d678fa916c41f24d
patsul12 <patsul12@gmail.com>
2016-01-06 18:53:07 -0800

    fixed triggering on valid comments

--- test/loop-protect.test.js
+++ test/loop-protect.test.js
@@ -47,6 +47,7 @@ var code = {
   cs: 'var bar, foo;\n\nfoo = function(i) {\n  return {\n    id: i\n  };\n};\n\nbar = function(i) {\n\n  var j, _i, _results;\n\n  _results = [];\n  for (j = _i = 1; 1 <= i ? _i < i : _i > i; j = 1 <= i ? ++_i : --_i) {\n    _results.push(j);\n  }\n  return _results;\n};',
   loopbehindif: 'if (false) {for (var i = 1; i--;) {throw Error;}}',
   badloopbehindif: 'if (false) for (var i = 1; i--;) {throw Error;}',
+  singlelinemultiline: '/* reverse palinWord doe comparison*/'
 };
 
 var spy;
@@ -284,6 +285,13 @@ describe('labels', function () {
     c = code.badloopbehindif;
     compiled = loopProtect(c);
     assert(compiled !== c);
+    run(compiled);
+  })
+
+  it('should handle single line "/* comment */"', function() {
+    var c = code.singlelinemultiline
+    var compiled = loopProtect(c);
+    assert(compiled === c);
     run(compiled);
   })
 
