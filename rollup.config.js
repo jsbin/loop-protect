bbd96f98c289388234f90fdeff1322ff8a7d7a69
Tim Davis <7432943+thebearingedge@users.noreply.github.com>
2022-11-15 16:11:33 -0800

    set up project

---
+++ rollup.config.js
@@ -0,0 +1,23 @@
+import { defineConfig } from 'rollup'
+import ts from '@rollup/plugin-typescript'
+
+export default defineConfig({
+  input: 'src/loop-protect.ts',
+  output: [
+    {
+      file: 'dist/cjs/loop-protect.js',
+      format: 'cjs',
+      exports: 'auto'
+    },
+    {
+      file: 'dist/esm/loop-protect.js',
+      format: 'esm'
+    },
+    {
+      file: 'dist/umd/loop-protect.js',
+      format: 'umd',
+      name: 'loopProtect'
+    }
+  ],
+  plugins: [ts()]
+})
